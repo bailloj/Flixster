594527ee5e6f07797157970d5573f148c68bfcbe
administrator <administrator@4316.local>
2019-02-10 17:26:53 -0800

    Initial Commit

---
+++ Flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flixster
+//
+//  Created by administrator on 2/10/19.
+//  Copyright © 2019 CodePathProgram2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
